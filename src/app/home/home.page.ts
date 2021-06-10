90c239e7e35e8f67a9bf31aab1f8043504871e09
Mariam de Jesus Delgado Zerquera <mariam.delgado4258@alumnos.udg.mx>
2021-06-10 16:30:31 -0500

    commit con lo basico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlbumsService } from '../api/albums.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+  albums =[];
+  //   {
+  //     titulo: "Four",
+  //     anioPublicacion: "2014",
+  //     artista: "One Direction",
+  //     foto: "https://images-na.ssl-images-amazon.com/images/I/714lCb7-Y3L._SL1200_.jpg"
+  //   },
+  //   {
+  //     titulo: "Utopia",
+  //     anioPublicacion: "2019",
+  //     artista: "Romeo Santos",
+  //     foto: "https://resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/2925909.jpg"
+  //   }
+  // ];
+  constructor(private albumsService: AlbumsService) {}
+  ngOnInit(){
+    this.albumsService.albums.subscribe(albums => {
+      this.albums=albums;
+    });
+    this.albumsService.getAlbums();
+  }
 }
